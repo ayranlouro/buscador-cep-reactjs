b1848bef7f8a747328badab1e02350afd30f0660
ayran louro <ayranlouro@hotmail.com>
2022-11-07 18:44:37 -0300

    fix: errors handled to show on screen

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import "./styles.css";
 
-import api from './services/cep/api';
+import api from "./services/cep/api";
 
 function App() {
-
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [response, setResponse] = useState({});
+  const [errors, setErros] = useState();
 
-  async function handleSearch(){
-    if(input === ''){
-      alert('Prencha algum CEP');
+  async function handleSearch() {
+    if (input === "") {
+      alert("Prencha algum CEP");
       return;
     }
 
@@ -20,8 +20,11 @@ function App() {
       const response = await api.get(`${input}/json`);
       console.log(response.data);
       setResponse(response.data);
+      setErros("");
     } catch (error) {
-      alert('O CEP informado não existe.');
+      console.log(error);
+      setErros(error);
+      setResponse("");
     }
   }
 
@@ -30,22 +33,36 @@ function App() {
       <h1 className="title">Buscador CEP</h1>
 
       <div className="containerInput">
-        <input type="text" className="input" placeholder="Digite seu Cep" value={input} onChange={(e) => setInput(e.target.value)}/>
+        <input
+          type="text"
+          className="input"
+          placeholder="Digite seu Cep"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
 
         <button className="buttonSearch">
-          <FiSearch size={25} color="#FFF" onClick={handleSearch}/>
+          <FiSearch size={25} color="#FFF" onClick={handleSearch} />
         </button>
       </div>
 
-      <main className="main">
-        <h2>CEP: {response.cep}</h2>
+      {Object.keys(response).length > 0 && (
+        <main className="main">
+          <h2>CEP: {response.cep}</h2>
 
-        <span>Bairro: {response.bairro}</span>
-        <span>Rua: {response.logradouro}</span>
-        <span>Complemento: {response.complemento}</span>
-        <span>Localidade: {response.localidade}</span>
-        <span>{response.uf}</span>
+          <span>Bairro: {response.bairro}</span>
+          <span>Rua: {response.logradouro}</span>
+          <span>Complemento: {response.complemento}</span>
+          <span>Localidade: {response.localidade}</span>
+          <span>{response.uf}</span>
+        </main>
+      )}
+
+      {errors ? (
+        <main className="mainError">
+        <h2>{errors.message}</h2>
       </main>
+      ) : null}
     </div>
   );
 }
