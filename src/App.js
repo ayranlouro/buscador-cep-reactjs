1f43d371a4553bbd11a42aa4ec134b800b401175
ayran louro <ayranlouro@hotmail.com>
2022-11-07 17:42:25 -0300

    styles finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { FiSearch } from "react-icons/fi";
+import "./styles.css";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Teste</h1>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input type="text" className="input" placeholder="Digite seu Cep" />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 12837172</h2>
+
+        <span>Rua teste</span>
+        <span>Complemento: </span>
+        <span>Copacabana</span>
+        <span>Rio de Janeiro - RJ</span>
+      </main>
     </div>
   );
 }
